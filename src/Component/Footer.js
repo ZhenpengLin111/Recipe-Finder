53b8b6c933509e8c7a8c9403496e250e516b4151
ZPL123 <penglin524@gmail.com>
2024-03-20 12:10:15 -0400

    finished ingredients-search page styling for different devices and fixed footer styling problem

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,9 +1,10 @@
 // Footer.js
 import React from 'react';
+import '../Styles/Footer.css'
 
 const Footer = () => {
   return (
-    <footer style={{ color: 'white', backgroundColor: '#141010', padding: '10px', textAlign: 'center', position: 'relative', bottom: 0, width: '100%', display:'flex', flexDirection:'column', alignItems:'center',}}>
+    <footer style={{ }}>
       <p>&copy; 2023 <span style={{color:'blanchedalmond'}}>Recipe Finder</span>. All rights reserved.</p>
       <div className='underline' style={{position:'relative', height:'2px', width:'60%', background:'gray', marginBottom: '10px'}}></div>
     </footer>
